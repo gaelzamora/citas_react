2ac069f6d85b1f6ee366063564621b9558005c06
gaelzamora <jgaelhz@gmail.com>
2023-09-09 18:30:06 -0600

    No fue mucho trabajo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){ 
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimientos Pacientes{' '}
+            Seguimientos Pacientes con GitHub{' '}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
